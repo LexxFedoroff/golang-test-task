d6dab3423430ae19bac88a823a708632f79ddfb5
Alexey Fedorov <LexxFedoroff@gmail.com>
2018-08-11 14:33:16 +0300

    implement message loop

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"io"
+	"bufio"
+	"flag"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+
+	"github.com/nu7hatch/gouuid"
 )
+
+func generateUUID() *uuid.UUID {
+	u4, err := uuid.NewV4()
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	return u4
+}
+
+var appID = generateUUID()
+
+var port = flag.Int("port", 8000, "")
 
 func initGracefulStop() {
 	var gracefulStop = make(chan os.Signal)
@@ -24,12 +43,39 @@ func startDiscovering() {
 
 }
 
-func startMessageLoop() {
+func startMessageLoop(period time.Duration) {
+	tickChan := time.Tick(period)
+	for range tickChan {
+		log.Printf("Send messages to other instance")
 
+		otherPort := 8000
+		if otherPort == *port {
+			otherPort = 8001
+		}
+
+		conn, err := net.Dial("tcp", fmt.Sprintf(":%v", otherPort))
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		defer conn.Close()
+
+		fmt.Fprintf(conn, appID.String()+"\n")
+
+		bufReader := bufio.NewReader(conn)
+		responce, err := bufReader.ReadString('\n')
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		log.Print(responce)
+	}
 }
 
 func listen(handler func(net.Conn)) {
-	address := ":8000"
+	address := fmt.Sprintf(":%v", *port)
 
 	l, err := net.Listen("tcp", address)
 	if err != nil {
@@ -45,7 +91,7 @@ func listen(handler func(net.Conn)) {
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
 		}
 
 		go handler(conn)
@@ -53,27 +99,38 @@ func listen(handler func(net.Conn)) {
 }
 
 func handler(conn net.Conn) {
-	log.Printf("connection has been accepted")
+	log.Print("connection has been accepted")
 
 	defer func() {
 		conn.Close()
-		log.Printf("connection has been closed")
+		log.Print("connection has been closed")
 	}()
 
-	_, err := io.Copy(conn, conn)
+	bufReader := bufio.NewReader(conn)
+	request, err := bufReader.ReadString('\n')
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return
+	}
+
+	_, err = fmt.Fprintf(conn, fmt.Sprintf("Hello, %v\n", request))
+	if err != nil {
+		log.Print(err)
+		return
 	}
 }
 
 func main() {
+	flag.Parse()
+
 	log.SetOutput(os.Stdout)
-	log.Println("application is starting...")
+	log.Print("application is starting...")
+	log.Printf("Application UUID %v", appID)
 
 	initGracefulStop()
 
 	go startDiscovering()
-	go startMessageLoop()
+	go startMessageLoop(3 * time.Second)
 
 	listen(handler)
 }
